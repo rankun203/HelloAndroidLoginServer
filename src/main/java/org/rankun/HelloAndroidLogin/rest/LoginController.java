3bb89c1500a1401d9121e8c6bdb9b7900bc9b734
Kun Ran <rankun203@gmail.com>
2014-04-03 21:11:27 +0800

    modify web.xml /api/* -> /* attempt to fix Openshift 404

--- src/main/java/org/rankun/HelloAndroidLogin/rest/LoginController.java
+++ src/main/java/org/rankun/HelloAndroidLogin/rest/LoginController.java
@@ -1,15 +1,12 @@
 package org.rankun.HelloAndroidLogin.rest;
 
 import org.rankun.HelloAndroidLogin.rest.bean.Info;
-import org.rankun.HelloAndroidLogin.rest.bean.User;
-import org.rankun.HelloAndroidLogin.rest.data.UserDatabase;
 
 import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
 
 /**
  * Created by rankun203 on 14-3-28
@@ -25,49 +22,4 @@ public class LoginController {
         return new Info("success");
     }
 
-    @GET
-    @PermitAll
-    @Path("/users/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public User getUserById(@PathParam("id") int id) {
-        return UserDatabase.getUserById(id);
-    }
-    @GET
-    @PermitAll
-    @Path("/users")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getUsers() {
-        return UserDatabase.getAllUsers();
-    }
-
-    @PUT
-    @RolesAllowed("ADMIN")
-    @Path("/users/{id}")
-    public Response updateUserById(@PathParam("id") int id) {
-        //Update the User resource
-        UserDatabase.updateUser(id);
-        return Response.status(200).build();
-    }
-    @POST
-    @RolesAllowed("ADMIN")
-    @Path("/users")
-    public Response addUser(@FormParam("username") String username,
-                            @FormParam("password") String password) {
-
-        System.out.println(username == null);
-        System.out.println(password == null);
-
-        if (null == username || null == password) {
-            return Response.status(404).build();
-        }
-
-        boolean success = UserDatabase.addUser(username, password);
-
-
-        if (success) {
-            return Response.status(200).build();
-        } else {
-            return Response.status(409).build();
-        }
-    }
 }
---
+++ src/main/java/org/rankun/HelloAndroidLogin/rest/UserController.java
@@ -0,0 +1,61 @@
+package org.rankun.HelloAndroidLogin.rest;
+
+import org.rankun.HelloAndroidLogin.rest.bean.User;
+import org.rankun.HelloAndroidLogin.rest.data.UserDatabase;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+/**
+ * Created by rankun203 on 14-4-3
+ */
+@Path("/api/users")
+public class UserController {
+    @GET
+    @PermitAll
+    @Path("/users/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUserById(@PathParam("id") int id) {
+        return UserDatabase.getUserById(id);
+    }
+    @GET
+    @PermitAll
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getUsers() {
+        return UserDatabase.getAllUsers();
+    }
+
+    @PUT
+    @RolesAllowed("ADMIN")
+    @Path("/users/{id}")
+    public Response updateUserById(@PathParam("id") int id) {
+        //Update the User resource
+        UserDatabase.updateUser(id);
+        return Response.status(200).build();
+    }
+    @POST
+    @RolesAllowed("ADMIN")
+    @Path("/users")
+    public Response addUser(@FormParam("username") String username,
+                            @FormParam("password") String password) {
+
+        if (null == username || null == password) {
+            return Response.status(404).build();
+        }
+
+        boolean success = UserDatabase.addUser(username, password);
+
+
+        if (success) {
+            return Response.status(200).build();
+        } else {
+            return Response.status(409).build();
+        }
+    }
+
+}
